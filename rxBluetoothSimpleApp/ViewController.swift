676e139903df38b3ebd59ba0013d200e854506fd
Christian Westesson <chris.westesson@gmail.com>
2022-08-12 10:17:34 +0200

    Initial Commit

---
+++ rxBluetoothSimpleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rxBluetoothSimpleApp
+//
+//  Created by ChristianWestesson on 2022-08-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
